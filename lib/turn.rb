6bff71a380acbda40f389918014bdf276d9511d8
Jennifer Hong <jennifer.hong13@gmail.com>
2018-06-07 20:40:55 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,13 +7,12 @@ def display_board(board)
 end
 
 def input_to_index(input)
-  converted_input = input.to_i-1
+  user_input = input.to_i-1
 end
 
 def move(board, position, player = "X")
   board[position] = player
 end
-
 
 def valid_move?(board, index)
   if index.between?(0,8)
